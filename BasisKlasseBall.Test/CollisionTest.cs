9ee4d262d85a3609b4c79967df383770ac2eeacc
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-10-21 11:46:38 +0200

    Add files via upload

---
+++ BasisKlasseBall.Test/CollisionTest.cs
@@ -0,0 +1,26 @@
+﻿using BasisKlasseBall;
+using Xunit;
+
+namespace BasisKlasseBall.Test
+{
+    public class CollisionTest
+    {
+        [Fact]
+        public void CheckHit_BallShouldCollideWithPlayer()
+        {
+            //arrange
+            bool expected = true;
+
+            //act
+            //int expectedPlayerX = 5;
+            //int expectedBallX = 5;
+            //int expectedPlayerY = 5;
+            //int expectedBallY = 5;
+            bool actual = true;
+            // bool actual = (expectedPlayerX == expectedBallX) && (expectedPlayerY == expectedBallY);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
